b90c5328acf6d34e81d16562e361c6a29a31b1b5
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-07-22 16:27:01 -0300

    Added simple button

---
+++ src/app/components/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./Button";
+
+const ButtonPage = () => {
+  return (
+    <div className="w-96 h-96 justify-center flex border-cyan-400  border-2 items-center">
+      <Button></Button>
+    </div>
+  );
+};
+
+export default ButtonPage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Link from "next/link";
+
+const UserPage = () => {
+  return (
+    <>
+      <Link href="/"> Go back</Link>
+    </>
+  );
+};
+
+export default UserPage;
