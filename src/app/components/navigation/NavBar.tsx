ad94e572698450c71da6ca94e2f10719c69fd01e
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-07-24 16:17:21 -0300

    Added NavBar variants

--- src/app/components/navigation/NavBar.tsx
+++ src/app/components/navigation/NavBar.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Logo } from "../Logo";
 import Button from "../Button";
+import clsx from "clsx";
 
-export const NavBar = () => {
+
+interface NavbarProps {
+  variant: keyof typeof navBarVariantsStyles;
+}
+
+const defaultStyles = "container mx-auto px-20 top-0 sticky flex py-4 justify-between items-center"
+const navBarVariantsStyles = {
+  default: defaultStyles,
+  dark: clsx(defaultStyles, "bg-dark-green-900"),
+};
+
+export const NavBar = ({ variant = "default" }: NavbarProps) => {
   return (
-    <div className="flex py-4 justify-center items-center">
-      <Logo></Logo>
-      <div className="flex">
+    <div className={navBarVariantsStyles[variant]}>
+      <Logo variant="default"></Logo>
+      <div className="flex gap-x-6">
         <Button variant="noBackground" text="Home" />
         <Button variant="noBackground" text="About" />
         <Button variant="noBackground" text="Contact" />
