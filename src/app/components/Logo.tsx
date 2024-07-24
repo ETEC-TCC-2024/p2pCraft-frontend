83fc9682cea87e640d65bd51eb94358da41fffec
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-07-24 16:18:00 -0300

    Added Logo variants

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import LogoIcon from "./icon/LogoIcon";
+import clsx from "clsx";
 
-export const Logo = () => {
+interface LogoProps {
+  variant: keyof typeof LogoVariantStyles
+}
+
+const defaultStyles = "flex items-center justify-center max-w-fit"
+const LogoVariantStyles = {
+  default: defaultStyles,
+  textWhite: clsx(defaultStyles, "text-white-50")
+}
+
+
+export const Logo = ({variant = "default"} : LogoProps) => {
   return (
-    <div className="flex items-center justify-center max-w-fit">
+    <div className={LogoVariantStyles[variant]}>
       <LogoIcon width={46} height={46}></LogoIcon>
       <div className="font-medium text-base">P2PCraft</div>
     </div>
