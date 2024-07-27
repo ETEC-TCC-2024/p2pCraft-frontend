143dc79122b5af55d91bf2e281aace2d352ed313
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-07-27 09:45:22 -0300

    refactor: Remove onClick and text button props and use them as the children prop

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -2,39 +2,31 @@
 import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 import clsx from "clsx";
-import React, { ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes, Children } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {
-  onclick?: () => void,
-  text: string
-}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariants> {}
 
 //TODO: fix the texcolor, the primary button text color may not be black
 
-export const buttonVariants = cva("text-base py-4 rounded-lg ps-5 pe-5 font-bold",
-  {
-    variants: {
-      variant: {
-        primary: "bg-grey-900 hover:bg-grey-500 text-white-50",
-        link: " hover:text-grey-500 hover:no-underline font-bold underline",
-        green: "bg-dark-green-700 hover:bg-dark-green-600 ",
-        noBackground: "text-opacity-50 hover:text-opacity-100 font-medium",
-      },
-      textColor: {
-        white: "text-white-50",
-        black: "text-black-900"
-      }
-    }
-  }
-)
+export const buttonVariants = cva("text-base py-4 rounded-lg ps-5 pe-5 font-bold", {
+  variants: {
+    variant: {
+      primary: "bg-grey-900 hover:bg-grey-500 text-white-50",
+      link: " hover:text-grey-500 hover:no-underline font-bold underline",
+      green: "bg-dark-green-700 hover:bg-dark-green-600 ",
+      noBackground: "text-opacity-50 hover:text-opacity-100 font-medium",
+    },
+    textColor: {
+      white: "text-white-50",
+      black: "text-black-900",
+    },
+  },
+});
 
-export const Button: React.FC<ButtonProps> = ({ variant = "primary", text = "Button", textColor = "white", onclick = () => { } }) => {
-  clsx();
+export const Button: React.FC<ButtonProps> = ({ variant = "primary", textColor = "white", children }) => {
   return (
     <div className="flex ml-auto mr-auto">
-      <button onClick={onclick} className={cn(buttonVariants({ variant, textColor }))}>
-        {text}
-      </button>
+      <button className={cn(buttonVariants({ variant, textColor }))}>{children}</button>
     </div>
   );
 };
