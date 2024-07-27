1234ef52c5931821e159e32141c8d6c0e6e03535
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-07-27 09:23:13 -0300

    refactor: Change Navbar component variants to cva

--- src/app/components/navigation/NavBar.tsx
+++ src/app/components/navigation/NavBar.tsx
@@ -1,28 +1,39 @@
 import React from "react";
-import { Logo } from "../Logo";
-import Button from "../Button";
-import clsx from "clsx";
+import { Logo, logoVariants } from "../Logo";
+import Button, { buttonVariants } from "../Button";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 
 
-interface NavbarProps {
-  variant: keyof typeof navBarVariantsStyles;
+interface NavbarProps extends VariantProps<typeof navbarVariants> {
 }
 
-const defaultStyles = "container mx-auto px-20 top-0 sticky flex py-4 justify-between items-center"
-const navBarVariantsStyles = {
-  default: defaultStyles,
-  dark: clsx(defaultStyles, `bg-dark-green-900`),
-};
+const navbarVariants = cva("container mx-auto px-4 top-0  py-1 sticky flex justify-between items-center", {
+  variants: {
+    variant: {
+      primary: "",
+      dark: "bg-dark-green-900"
+    },
+  },
+})
 
-export const NavBar = ({ variant = "default" }: NavbarProps) => {
+
+export const NavBar: React.FC<NavbarProps> = ({ variant = "primary" }: NavbarProps) => {
+  let buttonVariant: VariantProps<typeof buttonVariants>["textColor"];
+  if (variant === 'dark') {
+    buttonVariant = "white";
+  } else {
+    buttonVariant = "black"
+  }
+
   return (
-    <div className={navBarVariantsStyles[variant]}>
-      <Logo variant="primary"></Logo>
-      <div className="flex gap-x-6">
-        <Button variant="noBackground" text="Home" />
-        <Button variant="noBackground" text="About" />
-        <Button variant="noBackground" text="Contact" />
-        <Button variant="noBackground" text="FAQ" />
+    <div className={cn(navbarVariants({ variant }))}>
+      <Logo textVariant={variant == "dark" ? "white" : "primary"}></Logo>
+      <div className="md:flex gap-x-6">
+        <Button variant="noBackground" text="Home" textColor={buttonVariant} />
+        <Button variant="noBackground" text="About" textColor={buttonVariant} />
+        <Button variant="noBackground" text="Contact" textColor={buttonVariant} />
+        <Button variant="noBackground" text="FAQ" textColor={buttonVariant} />
         <Button variant="primary" text="Play" />
       </div>
     </div>
--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Button } from "./Button";
+import { NavBar } from "./navigation/NavBar";
 
 const ButtonPage = () => {
   return (
-    <div className="w-96 h-96 justify-center flex border-cyan-400  border-2 items-center">
-      <Button></Button>
-    </div>
+    <>
+      <NavBar variant="primary"></NavBar>
+    </>
   );
 };
 
