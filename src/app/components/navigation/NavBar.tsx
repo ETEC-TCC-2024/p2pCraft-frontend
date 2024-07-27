839588962be662fd34084c95d8c290a4258c90ae
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-07-27 09:46:23 -0300

    refactor: Remove the text from the button prop and added it as a button child

--- src/app/components/navigation/NavBar.tsx
+++ src/app/components/navigation/NavBar.tsx
@@ -4,37 +4,47 @@ import Button, { buttonVariants } from "../Button";
 import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-
-interface NavbarProps extends VariantProps<typeof navbarVariants> {
-}
+interface NavbarProps extends VariantProps<typeof navbarVariants> {}
 
 const navbarVariants = cva("container mx-auto px-4 top-0  py-1 sticky flex justify-between items-center", {
   variants: {
     variant: {
       primary: "",
-      dark: "bg-dark-green-900"
+      dark: "bg-dark-green-900",
     },
   },
-})
-
+});
 
 export const NavBar: React.FC<NavbarProps> = ({ variant = "primary" }: NavbarProps) => {
   let buttonVariant: VariantProps<typeof buttonVariants>["textColor"];
-  if (variant === 'dark') {
+  if (variant === "dark") {
     buttonVariant = "white";
   } else {
-    buttonVariant = "black"
+    buttonVariant = "black";
   }
 
   return (
     <div className={cn(navbarVariants({ variant }))}>
       <Logo textVariant={variant == "dark" ? "white" : "primary"}></Logo>
       <div className="md:flex gap-x-6">
-        <Button variant="noBackground" text="Home" textColor={buttonVariant} />
-        <Button variant="noBackground" text="About" textColor={buttonVariant} />
-        <Button variant="noBackground" text="Contact" textColor={buttonVariant} />
-        <Button variant="noBackground" text="FAQ" textColor={buttonVariant} />
-        <Button variant="primary" text="Play" />
+       
+        <Button variant="noBackground" textColor={buttonVariant}>
+          Home
+        </Button>
+
+        <Button variant="noBackground" textColor={buttonVariant}>
+          About
+        </Button>
+
+        <Button variant="noBackground" textColor={buttonVariant}>
+          Contact
+        </Button>
+
+        <Button variant="noBackground" textColor={buttonVariant}>
+          FAQ
+        </Button>
+
+        <Button variant="primary">Play</Button>
       </div>
     </div>
   );
