24b1166e2f3281fd4654015462d54ff42f63a576
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-08-02 18:22:49 -0300

    fix: Fix Navbar height

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { NavBar } from "./navigation/NavBar";
+import { Footer } from "./navigation/Footer";
 
 const ButtonPage = () => {
   return (
     <>
       <NavBar variant="primary"></NavBar>
+      <h1 className="w-1 min-h-[1060px]">|</h1>
+      <Footer></Footer>
     </>
   );
 };
