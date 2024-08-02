65fb808f66a3e34d47f479a275c684f800495444
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-08-02 18:22:12 -0300

    feat: Add color props to footer

--- src/app/components/navigation/Footer.tsx
+++ src/app/components/navigation/Footer.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import LogoIcon from "../icon/LogoIcon";
 import Button from "../Button";
-import { link } from "fs";
+import { cva, VariantProps } from "class-variance-authority";
 
-export const Footer = () => {
+
+interface FooterProps extends VariantProps<typeof footerVariants> {}
+
+const footerVariants = cva(" container flex mx-auto mt-auto items-center justify-between px-4 w-full", {
+  variants: {
+    variant: {
+      primary: "",
+      dark: "bg-green-ion-800",
+    },
+  },
+});
+export const Footer : React.FC<FooterProps> = ({variant = "primary"} : FooterProps) => {
+  
   //TODO replace logoIcon with the actuall icon
   return (
-    <div className="bg-green-ion-800 contianer flex mx-auto items-center justify-between px-4 bottom-0 w-full">
+    <div className="bg-green-ion-800 container flex mx-auto mt-auto items-center justify-between px-4 w-full">
       <FooterLogo></FooterLogo>
       <div className="flex justify-between w-full">
         <Button variant="link">Sobre mim</Button>
@@ -25,7 +37,7 @@ export const Footer = () => {
 export const FooterLogo = () => {
   return (
     <>
-      <div className="flex items-center ">
+      <div className="flex items-center">
         <LogoIcon width={64} height={64} />
         <div className="min-w-fit text-white-50">© 2024 P2PCraft</div>
       </div>
