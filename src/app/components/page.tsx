08e334ee877ff7986b7ea6de5ae31afea8e3d800
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-08-10 10:28:59 -0300

    refactor: Remove width and height properties and added className property instead

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { NavBar } from "./navigation/NavBar";
 import { Footer } from "./navigation/Footer";
+import LogoIcon from "./icon/LogoIcon";
+import Button from "./Button";
 
 const ButtonPage = () => {
   return (
     <>
       <NavBar variant="primary"></NavBar>
-      <h1 className="w-1 min-h-[1060px]">|</h1>
+      <BackgroundLogo></BackgroundLogo>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+      <h1 className="w-1 min-h-[1600px]">|z x nc b z c bn z x n  bc z xm b czxmnbczxm ncbzxn bczmxb </h1>
       <Footer></Footer>
     </>
   );
 };
+
+const BackgroundLogo = () => {
+  return (
+    <LogoIcon className="top-0 right-0 -z-10 absolute"></LogoIcon>
+  )
+}
 
 export default ButtonPage;
