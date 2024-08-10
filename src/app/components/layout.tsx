3f68dcb6f925280efb3708a99d70ac0c6222dc50
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-08-10 15:12:16 -0300

    feat: Make BackgroundLogo responsive (kinda)

--- src/app/components/layout.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react"
-export default function ComponentsLayout({ children }:
-    { children: React.ReactNode }) {
-
-    return <main className="bg-[#DCFFFB] -z-20 relative">{children} </main>
-
-}
