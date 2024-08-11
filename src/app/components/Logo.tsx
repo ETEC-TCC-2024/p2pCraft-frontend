cfcafe9ec7215d2faf9f3bef975e554c265a12fe
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "Added Logo variants"
    This reverts commit 41acbea547dc9748a5042bcae32a77d8e66bb634.

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 import LogoIcon from "./icon/LogoIcon";
-import clsx from "clsx";
 
-interface LogoProps {
-  variant: keyof typeof LogoVariantStyles
-}
-
-const defaultStyles = "flex items-center justify-center max-w-fit"
-const LogoVariantStyles = {
-  default: defaultStyles,
-  textWhite: clsx(defaultStyles, "text-white-50")
-}
-
-
-export const Logo = ({variant = "default"} : LogoProps) => {
+export const Logo = () => {
   return (
-    <div className={LogoVariantStyles[variant]}>
+    <div className="flex items-center justify-center max-w-fit">
       <LogoIcon width={46} height={46}></LogoIcon>
       <div className="font-medium text-base">P2PCraft</div>
     </div>
