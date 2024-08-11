efb3f198ed05e5af464e0aa3a19a09f4b1d79657
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "refactor: Change Button component variants to cva"
    This reverts commit da32c8a4bc798883d5cf47ef559d4439091ca3d9.

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -3,9 +3,9 @@ import LogoIcon from "./icon/LogoIcon";
 import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-interface LogoProps extends VariantProps<typeof logoVariants> { }
+interface LogoProps extends VariantProps<typeof logoVariants> {}
 
-export const logoVariants = cva("flex items-center max-w-fit font-bold text-base w-44", {
+const logoVariants = cva("flex items-center justify-center max-w-fit font-medium text-base", {
   variants: {
     variant: {
       primary: "",
@@ -17,11 +17,11 @@ export const logoVariants = cva("flex items-center max-w-fit font-bold text-base
   },
 });
 
-export const Logo: React.FC<LogoProps> = ({ variant, textVariant: text }) => {
+export const Logo: React.FC<LogoProps> = ({variant, textVariant: text}) => {
   return (
-    <div className={cn(logoVariants({ variant }))}>
+    <div className={cn(logoVariants({variant}))}>
       <LogoIcon width={46} height={46}></LogoIcon>
-      <div className={cn(logoVariants({ textVariant: text }))}>P2PCraft</div>
+      <div className={cn(logoVariants({textVariant: text}))}>P2PCraft</div>
     </div>
   );
 };
