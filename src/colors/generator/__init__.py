125a8db99d648ed30fd0e74c3959a3ed79af3190
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "script: unfuck"
    This reverts commit 6bc03dbc673220334aac485621280e7aa8c54baf.

--- src/colors/generator/__init__.py
+++ src/colors/generator/__init__.py
@@ -24,8 +24,7 @@ final_code += """   },
 },
 }"""
 
-#TODO rewrite this in js
-#TODO unfuck
-with open(file="tailwind.config.fuck", mode="+w") as final_file:
+#TODO make this add colors on a .ts file, also this should not be recreating the file everytime
+with open(file="tailwind.config.js", mode="+w") as final_file:
     final_file.write(final_code)
 
