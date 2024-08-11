f25c35a5c7a19a554ed86420d323722370a6a3d0
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "Fixed color generation script crashing when creating folders at src/colors"
    This reverts commit dbade157eb9d4de5620eb6cf06996523760b143c.

--- src/colors/generator/__init__.py
+++ src/colors/generator/__init__.py
@@ -7,12 +7,9 @@ module.exports = {
   theme: {
     colors: {"""
 
-colors_dir_path = "src/colors"
-for filename in os.listdir(colors_dir_path):
-    file_path = os.path.join(colors_dir_path, filename)
-    
-    if os.path.isdir(file_path): continue
-    
+
+for filename in os.listdir("colors"):
+    file_path = os.path.join("colors", filename)
     with open(file=file_path) as file:
         parsed_json = loads(file.read())
         for json_obj in parsed_json:
@@ -24,7 +21,6 @@ final_code += """   },
 },
 }"""
 
-#TODO make this add colors on a .ts file, also this should not be recreating the file everytime
 with open(file="tailwind.config.js", mode="+w") as final_file:
     final_file.write(final_code)
 
