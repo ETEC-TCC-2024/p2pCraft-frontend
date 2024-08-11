7ef7b0bceeb4ce220ead83368e3bb7675d9292b7
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "Created simple button"
    This reverts commit ae2db508b0fe1804b9315c594f8ac46d4f915f45.

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,28 +1,15 @@
-"use client";
-import clsx from "clsx";
 import React from "react";
 
 interface ButtonProps {
-  variant: keyof typeof buttonVariantsStyles;
-  text?: string;
-  onclick?: () => void;
+  variant: "primary" | "link" | "green";
+  text: String;
+  state: "hover" | "default";
 }
 
-const defaultStyles = "text-base py-4 rounded-lg ps-5 pe-5 font-bold";
-const buttonVariantsStyles = {
-  primary: clsx(defaultStyles, "bg-grey-900 hover:bg-grey-500 text-white-50 "),
-  link: "text-base text-grey-900 hover:text-grey-500 hover:no-underline font-bold underline",
-  green: clsx(defaultStyles, "bg-dark-green-700 hover:bg-dark-green-600 text-white-50"),
-  noBackground: "text-grey-900 text-opacity-50 hover:text-opacity-100 font-medium",
-};
-
-export const Button = ({ variant = "primary", text = "Button", onclick = () => {} }: ButtonProps) => {
-  clsx();
+export const Button = ({}) => {
   return (
     <div className="flex ml-auto mr-auto">
-      <button onClick={onclick} className={buttonVariantsStyles[variant]}>
-        {text}
-      </button>
+      <div className="text-base py-4 rounded-lg ps-5 pe-5 bg-black text-white font-bold">Button</div>
     </div>
   );
 };
