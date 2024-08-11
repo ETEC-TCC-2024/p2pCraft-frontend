872b559c418b08518d8ec1e9f589460542cec486
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "fix: Fix icon width and height not applying to absolute components"
    This reverts commit 04e2f558ea3300cd787fcf2cd917129da4c03e31.

--- src/app/components/icon/icons/OpenSource.tsx
+++ src/app/components/icon/icons/OpenSource.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {IconProps} from "../Icon";
-const OpenSource: React.FC <IconProps > = ({className}) => <div><svg className={className}   viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+const OpenSource: React.FC <IconProps > = ({className}) => <div><svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3.92308 13.2238V11.7692L8.98603 9.64338V11.2343L5.64335 12.4825L5.6958 12.4056V12.5909L5.64335 12.5105L8.98603 13.7622V15.3497L3.92308 13.2238ZM13.6932 7.88812L11.3855 16.4615H9.92745L12.2351 7.88812H13.6932ZM19.6975 13.2238L14.6346 15.3497V13.7622L17.9773 12.5105L17.9283 12.5909V12.4056L17.9773 12.4825L14.6346 11.2343V9.64338L19.6975 11.7692V13.2238Z" fill="#8AD7CC"/>
 </svg>
 </div>
