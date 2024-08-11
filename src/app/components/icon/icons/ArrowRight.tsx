bfedafadbc6fe530f214b43c50ca86e1b0479a4a
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "feat: Add missing icon components"
    This reverts commit 6be1e126664d3b1f80db2d0066d1a75061446f37.

--- src/app/components/icon/icons/ArrowRight.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {IconProps} from "../Icon";
-const ArrowRight: React.FC <IconProps > = ({className}) => <div><svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M16.7803 11.7195L9.28027 4.21967C8.98742 3.92677 8.51252 3.92677 8.21961 4.21972C7.92676 4.51262 7.92676 4.98747 8.21966 5.28037L15.1893 12.2499L8.21961 19.2197C7.92676 19.5127 7.92676 19.9875 8.21966 20.2804C8.36606 20.4269 8.55802 20.5001 8.74997 20.5001C8.94192 20.5001 9.13387 20.4269 9.28032 20.2803L16.7803 12.7801C16.921 12.6395 17 12.4487 17 12.2498C17 12.0509 16.921 11.8601 16.7803 11.7195Z" fill="#00A991"/>
-</svg>
-</div>
-export default ArrowRight;
