ac009034f77c68a6ead663ea9bccf6403df16179
Jorge Seu Churrasco com <73082261+seujorgenochurras@users.noreply.github.com>
2024-08-11 19:01:29 -0300

    Revert "feat: Add background color in page layout"
    This reverts commit 2f749da2d674dd87efc005cd27b50e42adc50abc.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen m-0 grid grid-cols-1">{children}</body>
     </html>
   );
 }
