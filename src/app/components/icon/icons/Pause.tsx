6be1e126664d3b1f80db2d0066d1a75061446f37
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-08-11 18:31:26 -0300

    feat: Add missing icon components

---
+++ src/app/components/icon/icons/Pause.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {IconProps} from "../Icon";
+const Pause: React.FC <IconProps > = ({className}) => <div><svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="5" y="5" width="4.85333" height="14" rx="2.42667" fill="#00473D"/>
+<rect x="14.3333" y="5" width="4.66667" height="14" rx="2.33333" fill="#00473D"/>
+</svg>
+</div>
+export default Pause;
---
+++ src/app/components/icon/icons/Stop.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {IconProps} from "../Icon";
+const Stop: React.FC <IconProps > = ({className}) => <div><svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="8" y="8" width="8" height="8" rx="1" fill="#094335"/>
+</svg>
+</div>
+export default Stop;
