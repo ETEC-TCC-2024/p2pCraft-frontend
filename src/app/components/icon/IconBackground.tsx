6f419827b06a4903d449082f24cde9684afa13a9
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-08-23 21:26:15 -0300

    fix: Fix icon height was broken

--- src/app/components/icon/IconBackground.tsx
+++ src/app/components/icon/IconBackground.tsx
@@ -1,29 +1,36 @@
 import { cva, VariantProps } from "class-variance-authority";
-import React, { Children, HtmlHTMLAttributes, ReactElement, useRef } from "react";
-import Icon, { IconProps } from "./Icon";
+import React from "react";
+import Icon from "./Icon";
 import { cn } from "@/lib/utils";
 
 interface IconBackgroundProps extends VariantProps<typeof variants> {
-    iconName: keyof typeof Icon,
-    className?: string
+  iconName: keyof typeof Icon;
+  className?: string;
 }
-const variants = cva("min-w-6 min-h-6", {
-    variants: {
-        variant: {
-            rounded: "rounded-full",
-            halfRounded: "rounded-2xl"
-        },
-        theme: {
-            dark: "bg-green-ion-900",
-            default: "bg-green-ion-300"
-        }
-    }
-})
-const IconBackground: React.FC<IconBackgroundProps> = ({ iconName, theme = "dark", variant = "halfRounded", className = "" }) => {
-    const IconComponent = Icon[iconName]
-    return <div className={cn(variants({ theme, variant }), className)}>
-        <IconComponent />
+const variants = cva("w-6 h-6", {
+  variants: {
+    variant: {
+      rounded: "rounded-full",
+      halfRounded: "rounded-2xl",
+    },
+    theme: {
+      dark: "bg-green-ion-900",
+      default: "bg-green-ion-300",
+    },
+  },
+});
+const IconBackground: React.FC<IconBackgroundProps> = ({
+  iconName,
+  theme = "dark",
+  variant = "halfRounded",
+  className = "",
+}) => {
+  const IconComponent = Icon[iconName];
+  return (
+    <div className={cn(variants({ theme, variant }), className)}>
+      <IconComponent />
     </div>
-}
+  );
+};
 
 export default IconBackground;
