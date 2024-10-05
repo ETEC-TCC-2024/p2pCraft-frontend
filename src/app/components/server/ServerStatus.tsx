fac9e13dfab02dcc375525e31d5e620722924f8e
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-10-05 17:45:23 -0300

    style: Format code

--- src/app/components/server/ServerStatus.tsx
+++ src/app/components/server/ServerStatus.tsx
@@ -1,51 +1,66 @@
-import React from "react"
-import Server from "./Server"
-import Text from "../text/TextComponent"
-import { cn } from "@/lib/utils"
-import IconBackground from "../icon/IconBackground"
-import Link from "next/link"
+import React from "react";
+import Server from "./Server";
+import Text from "../text/TextComponent";
+import { cn } from "@/lib/utils";
+import IconBackground from "../icon/IconBackground";
+import Link from "next/link";
 
-
-type ServerStatuses = "online" | "offline"
+type ServerStatuses = "online" | "offline";
 
 interface ServerStatusProps {
-    server: Server,
-    serverStatus: ServerStatuses
+  server: Server;
+  serverStatus: ServerStatuses;
 }
 
 const ServerStatus: React.FC<ServerStatusProps> = ({ server, serverStatus }) => {
-    const textDefaultClassName = "text-white-50 font-semibold font-sans"
-    return <Link href={`/server/${server.name}`}>
-        <div className={cn("w-96 bg-dark-green-600 flex gap-2 pr-3 justify-between hover:bg-gradient-to-r from-[1%] to-20% to-bg-dark-green-600", {
+  const textDefaultClassName = "text-white-50 font-semibold font-sans";
+  return (
+    <Link href={`/server/${server.name}`}>
+      <div
+        className={cn(
+          "w-96 bg-dark-green-600 flex gap-2 pr-3 justify-between hover:bg-gradient-to-r from-[1%] to-20% to-bg-dark-green-600",
+          {
             "from-green-ion-500/50": serverStatus === "online",
-            "from-red-500/50": serverStatus === "offline"
-        })}>
-            <StatusBar status={serverStatus} />
-            <div className="flex flex-1 flex-col my-4">
-                <Text className={cn(textDefaultClassName, "text-3xl")}>{server.name}</Text>
-                <Text className={textDefaultClassName}>{server.minecraftVersion}</Text>
-            </div>
-            <StatusIcon status={serverStatus}></StatusIcon>
+            "from-red-500/50": serverStatus === "offline",
+          }
+        )}
+      >
+        <StatusBar status={serverStatus} />
+        <div className="flex flex-1 flex-col my-4">
+          <Text className={cn(textDefaultClassName, "text-3xl")}>{server.name}</Text>
+          <Text className={textDefaultClassName}>{server.minecraftVersion}</Text>
         </div>
+        <StatusIcon status={serverStatus}></StatusIcon>
+      </div>
     </Link>
-}
+  );
+};
 
 const StatusBar: React.FC<{ status: ServerStatuses }> = ({ status }) => {
-    return <div className={cn("w-3", {
+  return (
+    <div
+      className={cn("w-3", {
         "bg-red-500": status === "offline",
-        "bg-green-ion-400": status === "online"
-    })}>
-    </div >
-}
+        "bg-green-ion-400": status === "online",
+      })}
+    ></div>
+  );
+};
 
 const StatusIcon: React.FC<{ status: ServerStatuses }> = ({ status }) => {
-    return <div className="my-auto">
-        <IconBackground iconName={status == "online" ? "Play" : "Stop"} theme={"dark"} variant={"rounded"} className={cn("h-12 w-12", {
-            "bg-green-ion-500": status === "online",
-            "bg-red-500": status === "offline"
-        })} />
+  return (
+    <div className="my-auto">
+      <IconBackground
+        iconName={status == "online" ? "Play" : "Stop"}
+        theme={"dark"}
+        variant={"rounded"}
+        className={cn("h-12 w-12", {
+          "bg-green-ion-500": status === "online",
+          "bg-red-500": status === "offline",
+        })}
+      />
     </div>
-}
+  );
+};
 
-
-export default ServerStatus
+export default ServerStatus;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,45 +3,69 @@ import LabelAndField from "../components/form/LabelAndField";
 import { Footer } from "../components/navigation/Footer";
 import { NavBar } from "../components/navigation/NavBar";
 import Button from "../components/button/Button";
-import Text from "../components/text/TextComponent";
-import Link from "next/link";
 import { register } from "../actions/auth";
 
-
 const RegisterPage = () => {
-    return <>
-        <header><NavBar variant={"primary"} /></header>
-        <div className="flex flex-col justify-center items-center ">
-            <form className="flex flex-col justify-center items-center px-9 py-12" action={register}>
-                <div className="flex flex-row gap-x-9">
-                    <DefaultLabelAndField labelName="name" fieldType="text" labelText="Nome"></DefaultLabelAndField>
-                    <DefaultLabelAndField labelName="email" fieldType="text" labelText="Email"></DefaultLabelAndField>
-                </div>
+  return (
+    <>
+      <header>
+        <NavBar variant={"primary"} />
+      </header>
+      <div className="flex flex-col justify-center items-center ">
+        <form className="flex flex-col justify-center items-center px-9 py-12" action={register}>
+          <div className="flex flex-row gap-x-9">
+            <DefaultLabelAndField
+              labelName="name"
+              fieldType="text"
+              labelText="Nome"
+            ></DefaultLabelAndField>
+            <DefaultLabelAndField
+              labelName="email"
+              fieldType="text"
+              labelText="Email"
+            ></DefaultLabelAndField>
+          </div>
 
-                <div className="flex flex-row gap-x-9">
-                    <DefaultLabelAndField labelName="pass" fieldType="text" labelText="Senha"></DefaultLabelAndField>
-                    <DefaultLabelAndField labelName="confirmPass" fieldType="text" labelText="Confirmar senha"></DefaultLabelAndField>
-                </div>
-                <Button variant={"green"} type="submit" className="mt-24">Registrar</Button>
-            </form>
-        </div>
-        <Footer variant="primary" />
+          <div className="flex flex-row gap-x-9">
+            <DefaultLabelAndField
+              labelName="pass"
+              fieldType="text"
+              labelText="Senha"
+            ></DefaultLabelAndField>
+            <DefaultLabelAndField
+              labelName="confirmPass"
+              fieldType="text"
+              labelText="Confirmar senha"
+            ></DefaultLabelAndField>
+          </div>
+          <Button variant={"green"} type="submit" className="mt-24">
+            Registrar
+          </Button>
+        </form>
+      </div>
+      <Footer variant="primary" />
     </>
-}
+  );
+};
 
 interface LabelAndFieldProps {
-    labelText: string,
-    labelName: string,
-    fieldType?: HTMLInputTypeAttribute
+  labelText: string;
+  labelName: string;
+  fieldType?: HTMLInputTypeAttribute;
 }
-const DefaultLabelAndField: React.FC<LabelAndFieldProps> = ({ labelName, labelText, fieldType = "text" }) => {
-    return <LabelAndField
-        className="min-w-[10%] max-w-[100%] w-96 mt-9"
-        fieldType={fieldType}
-        inputName={labelName}>
-        <div className="pl-1 p-2">
-            {labelText}
-        </div>
-    </LabelAndField >
-}
-export default RegisterPage;   
+const DefaultLabelAndField: React.FC<LabelAndFieldProps> = ({
+  labelName,
+  labelText,
+  fieldType = "text",
+}) => {
+  return (
+    <LabelAndField
+      className="min-w-[10%] max-w-[100%] w-96 mt-9"
+      fieldType={fieldType}
+      inputName={labelName}
+    >
+      <div className="pl-1 p-2">{labelText}</div>
+    </LabelAndField>
+  );
+};
+export default RegisterPage;
