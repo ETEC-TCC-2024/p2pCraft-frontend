10f972fceb6b72e30a652dc54004a02c0e7c430b
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-10-05 17:47:35 -0300

    style: Format code

--- src/lib/Session.ts
+++ src/lib/Session.ts
@@ -3,7 +3,6 @@ import { cookies } from "next/headers";
 import "server-only";
 
 export async function createSession(token: string) {
-  console.log(token);
   const jwt = decodeJwt(token);
   const expirestAt = new Date(0);
   expirestAt.setSeconds(jwt.exp!);
