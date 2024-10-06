4bd73a37ffedd52faa921e0a539536abcee30ee5
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-10-06 17:18:39 -0300

    feat: Create label variants

--- src/app/components/form/LabelAndField.tsx
+++ src/app/components/form/LabelAndField.tsx
@@ -1,27 +1,52 @@
-"use client"
-import TextField from "./TextField"
-import React, { HTMLInputTypeAttribute, ReactNode, useState } from "react"
-import { cn } from "@/lib/utils"
+"use client";
+import TextField, { fieldStates } from "./TextField";
+import React, { HTMLInputTypeAttribute, ReactNode, useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
 
 interface LabelAndFieldProps {
-    children?: ReactNode,
-    fieldType?: HTMLInputTypeAttribute,
-    inputName: string,
-    className?: string
+  children?: ReactNode;
+  fieldType?: HTMLInputTypeAttribute;
+  inputName: string;
+  className?: string;
+  fieldVariant?: fieldStates;
 }
 
+const LabelAndFieldVariants = cva("text-lg capitalize hover: hover:text-blue-300", {
+  variants: {
+    variant: {
+      focus: "text-blue-600 transition-all duration-200",
+      hover: "",
+      primary: "",
+      disable: "",
+      invalid: "text-red-400",
+    },
+  },
+});
 
-const LabelAndField: React.FC<LabelAndFieldProps> = ({ children, inputName, fieldType, className = "" }) => {
-    const [focus, setFocus] = useState(false)
-    return (
-        <label className={cn("text-lg capitalize hover:text-blue-300", { "text-blue-600 transition-all duration-200": focus }, className)}
-            onFocus={e => setFocus(true)} onBlur={e => setFocus(false)}>
-            {children}
-            <TextField name={inputName} type={fieldType} onFocus={e => setFocus(true)} onBlur={e => setFocus(false)}
-                className={cn("hover:border-blue-400",
-                    { "border-2 -m-[1px] border-blue-400 shadow-2xl border-opacity-100 transition-shadow duration-300": focus })} />
-        </label>
-    )
-}
+const LabelAndField: React.FC<LabelAndFieldProps> = ({
+  children,
+  inputName,
+  fieldType,
+  className = "",
+  fieldVariant = "primary",
+}) => {
+  const [fieldState, setFieldState] = useState<fieldStates>(fieldVariant);
+  useEffect(() => {
+    setFieldState(fieldVariant ? fieldVariant : "primary");
+  }, [fieldVariant]);
+  return (
+    <label className={cn(className, LabelAndFieldVariants({ variant: fieldState }))}>
+      {children}
+      <TextField
+        name={inputName}
+        type={fieldType}
+        onFocus={(e) => setFieldState("focus")}
+        onBlur={(e) => setFieldState("primary")}
+        variant={fieldState}
+      />
+    </label>
+  );
+};
 
 export default LabelAndField;
--- src/app/components/form/TextField.tsx
+++ src/app/components/form/TextField.tsx
@@ -1,13 +1,35 @@
-import { cn } from "@/lib/utils"
-import { InputHTMLAttributes } from "react"
+import { cn } from "@/lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { InputHTMLAttributes } from "react";
 
+interface TextFieldProps
+  extends InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof TextFieldVariants> {}
 
-interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-}
+const TextFieldVariants = cva(
+  "h-fit w-full text-base text-black-900 rounded-lg border p-3 outline-none bg-opacity-0 bg-white-900 border-black-900 border-opacity-40 transition-all duration-200",
+  {
+    variants: {
+      variant: {
+        primary: "",
+        focus:
+          "border-2 -m-[1px] border-blue-400 shadow-2xl border-opacity-100 transition-shadow duration-300",
+        hover: "border-blue-400",
+        invalid: "border-red-400",
+      },
+    },
+  }
+);
+
+export type fieldStates = VariantProps<typeof TextFieldVariants>["variant"];
 
 const TextField: React.FC<TextFieldProps> = ({ ...props }) => {
-    return <input {...props} className={cn("h-fit w-full text-base text-black-900 rounded-lg border p-3 outline-none bg-opacity-0 bg-white-900 border-black-900 border-opacity-40 disabled:bg-opacity-20 disabled:text-opacity-40",
-        props.className)} />
-}
+  return (
+    <input
+      {...props}
+      className={cn(TextFieldVariants({ variant: props.variant }), props.className)}
+    />
+  );
+};
 
-export default TextField
+export default TextField;
