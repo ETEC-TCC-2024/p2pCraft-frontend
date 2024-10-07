18d2b3b0ed638def1336f7de9d7043825fcb4fee
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-10-06 21:51:00 -0300

    feat: Create axios instance

--- src/api/AxiosConfig.ts
+++ src/api/AxiosConfig.ts
@@ -2,14 +2,19 @@ import { API_URL } from "@/app/actions/config";
 import axios from "axios";
 import { cookies } from "next/headers";
 
-axios.interceptors.request.use(
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json' }
+
+})
+axiosInstance.interceptors.request.use(
   (config) => {
     const isApiRequest = config.baseURL == API_URL;
     const token = cookies().get("session")?.value;
 
     if (token) {
       if (isApiRequest) {
-        console.log("sim mano")
         config.headers.Authorization = `Bearer ${token}`;
       }
     }
@@ -19,3 +24,6 @@ axios.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+
+export default axiosInstance
--- src/api/P2PApi.ts
+++ src/api/P2PApi.ts
@@ -1,48 +1,33 @@
 import { API_URL } from "@/app/actions/config";
+import axiosInstance from "./AxiosConfig";
 
 class P2PApi {
   async post(path: string, body: Object) {
-    return fetch(API_URL + path, {
+    return axiosInstance.post(
+      path, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
       body: JSON.stringify(body),
     });
   }
 
   async get(path: string) {
-    return fetch(API_URL + path, {
+    return axiosInstance.get(path, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
     });
   }
 
   async put(path: string, body: Object) {
-    return fetch(API_URL + path, {
+    return axiosInstance.put(path, {
       method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
       body: JSON.stringify(body),
     });
   }
 
-  async delete(path: string, body: Object) {
-    return fetch(API_URL + path, {
+  async delete(path: string) {
+    return axiosInstance.delete(path, {
       method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
     });
   }
-  async fetcher(path: string, init: RequestInit) {
-    init.headers = { "Content-Type": "application/json" };
 
-    return fetch(API_URL + path, init);
-  }
 }
 export default new P2PApi();
