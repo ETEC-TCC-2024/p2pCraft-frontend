17b8c9d1980810f06b269ed69bac0debbe442ed2
seujorgenochurras <eusoulegaldemaiseverdadeeujuro@gmail.com>
2024-10-14 14:42:26 -0300

    style: Format user service

--- src/api/service/UserService.ts
+++ src/api/service/UserService.ts
@@ -13,37 +13,33 @@ class UserService {
     const json = await response.data;
     newUser.name = json["name"];
     newUser.email = json["email"];
-    newUser.uuid = json["uuid"]
+    newUser.uuid = json["uuid"];
 
-    const servers = getServers(json)
+    const servers = getServers(json);
     newUser.servers = servers;
 
     return newUser;
   }
 
   async login(loginDto: ClientLoginDto) {
-    const response = await P2PApi.post("/login", loginDto)
-    return response
+    const response = await P2PApi.post("/login", loginDto);
+    return response;
   }
 
   async register(registerDto: ClientRegisterDto) {
-    const response = await P2PApi.post("/signup", registerDto)
-    return response
+    const response = await P2PApi.post("/signup", registerDto);
+    return response;
   }
 
-  async getServerById(uuid: string) {
-
-  }
-
+  async getServerById(uuid: string) {}
 }
 
 function getServers(json: any) {
-  const serverAcesses = json["serverAccesses"]
-  const servers: UserServer[] = []
+  const serverAcesses = json["serverAccesses"];
+  const servers: UserServer[] = [];
   for (const access of serverAcesses) {
     const server = getServerAccess(access);
-    servers.push(server)
-
+    servers.push(server);
   }
   return servers;
 }
@@ -52,31 +48,30 @@ function getServerAccess(json: any) {
   const server = new UserServer();
   const mapConfig = new MapConfig();
 
-  const role: UserServerRoles = UserServerRoles[json["role"]] as unknown as UserServerRoles
-  const serverJson = json["server"]
-  const uuid = serverJson["uuid"]
-  const name = serverJson["name"]
-  const online = serverJson["online"]
-  const staticIp = serverJson["staticIp"]
+  const role: UserServerRoles = UserServerRoles[json["role"]] as unknown as UserServerRoles;
+  const serverJson = json["server"];
+  const uuid = serverJson["uuid"];
+  const name = serverJson["name"];
+  const online = serverJson["online"];
+  const staticIp = serverJson["staticIp"];
 
-  const mapConfigJson = serverJson["mapConfigurations"]
+  const mapConfigJson = serverJson["mapConfigurations"];
 
-  const mapURL = mapConfigJson["mapUrl"]
-  const version = mapConfigJson["version"]
-  const seed = mapConfigJson["seed"]
+  const mapURL = mapConfigJson["mapUrl"];
+  const version = mapConfigJson["version"];
+  const seed = mapConfigJson["seed"];
 
-  mapConfig.mapUrl = mapURL
-  mapConfig.seed = seed
-  mapConfig.version = version
+  mapConfig.mapUrl = mapURL;
+  mapConfig.seed = seed;
+  mapConfig.version = version;
 
   server.uuid = uuid;
   server.name = name;
   server.staticIp = staticIp;
-  server.online = online
+  server.online = online;
   server.userRole = role;
-  server.mapConfig = mapConfig
+  server.mapConfig = mapConfig;
 
   return server;
-
 }
 export default new UserService();
